fda108c58975bfcfccd2dae0c7492ae147189562
matthewmu <matthewmu@tencent.com>
2023-08-14 20:09:26 +0800

    return  dbclient

--- library/rdb/dbrepo.go
+++ library/rdb/dbrepo.go
@@ -6,35 +6,39 @@ import (
 
 // DBRepository  releated database base repository
 type DBRepository struct {
-	dbclient *DBClient
-	tables   []interface{}
+	Client *DBClient
+	tables []interface{}
 }
 
 // NewDBBaseRepository new repo
 func NewDBBaseRepository(dbclient *DBClient, tables []interface{}) DBRepository {
 	return DBRepository{
-		dbclient: dbclient,
-		tables:   tables,
+		Client: dbclient,
+		tables: tables,
 	}
 }
 
 // SyncTables sync tables to db
 func (repo *DBRepository) SyncTables() error {
-	return repo.dbclient.SyncTables(repo.tables)
+	return repo.Client.SyncTables(repo.tables)
 }
 
 // DropTables drop  tables from  db
 func (repo *DBRepository) DropTables() error {
-	return repo.dbclient.DB().Migrator().DropTable(repo.tables...)
+	return repo.Client.DB().Migrator().DropTable(repo.tables...)
 }
 
 // NewSessionMaker proxy session maker
 func (repo *DBRepository) NewSessionMaker(session Session,
 ) (Session, *SesionMaker) {
-	return NewSessionMaker(session, repo.dbclient)
+	return NewSessionMaker(session, repo.Client)
 }
 
 // IsRecordNotFound @Description: 判断err是否是"没有该记录"
 func (repo *DBRepository) IsRecordNotFound(err error) bool {
 	return err == gorm.ErrRecordNotFound
+}
+
+func (repo *DBRepository) GetClient() *DBClient {
+	return repo.Client
 }
