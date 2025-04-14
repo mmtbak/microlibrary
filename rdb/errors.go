9f68dd355779cd466a2f88dda6962c4690b12b2c
mmt <47457296+mmtbak@users.noreply.github.com>
2025-04-14 14:33:07 +0800

    add  error

--- rdb/errors.go
+++ rdb/errors.go
@@ -4,6 +4,12 @@ import "gorm.io/gorm"
 
 // IsRecordNotFound ...
 // @Description: 判断err是否是"没有该记录"
-func IsRecordNotFound(err error) bool {
+func IsErrRecordNotFound(err error) bool {
 	return err == gorm.ErrRecordNotFound
+}
+
+// IsErrDuplicatedKey return true if the error is a duplicate key error
+// @Description: 判断err是否是"主键冲突"
+func IsErrDuplicatedKey(err error) bool {
+	return err == gorm.ErrDuplicatedKey
 }
